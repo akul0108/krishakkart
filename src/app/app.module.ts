904955c7b337092a206605ae42685859cb45c0ae
akul0108 <bca.akhil@gmail.com>
2020-01-02 17:37:26 +0530

    BootStrapCDN added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SellerRegistrationComponent } from './Seller/seller-registration/seller-registration.component';
+import { SellerHeaderComponent } from './Seller/seller-header/seller-header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SellerRegistrationComponent,
+    SellerHeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
