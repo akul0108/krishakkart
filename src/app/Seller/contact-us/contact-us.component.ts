b5fa87811432e2fa9e0f7670be7feb68473d65a8
TRPS-Agra <akhil@trpsits.com>
2020-03-10 10:50:39 +0530

    Error -0 Resolved

--- src/app/Seller/contact-us/contact-us.component.ts
+++ src/app/Seller/contact-us/contact-us.component.ts
@@ -8,35 +8,35 @@ import { WebServiceService } from '../web-service.service';
   templateUrl: './contact-us.component.html',
   styleUrls: ['./contact-us.component.css']
 })
-export class ContactUsComponent implements OnInit, AfterViewInit {
+export class ContactUsComponent implements OnInit/*, AfterViewInit */{
 
   message_send : FormGroup;
 
-  @ViewChild('mapContainer', {static : false}) gmap : ElementRef;
-  map : google.maps.Map;
-  lat = 28.752429;
-  lng = 77.499160;
+  // @ViewChild('mapContainer', {static : false}) gmap : ElementRef;
+  // map : google.maps.Map;
+  // lat = 28.752429;
+  // lng = 77.499160;
 
-  coordinates = new google.maps.LatLng(this.lat, this.lng);
+  // coordinates = new google.maps.LatLng(this.lat, this.lng);
 
-  mapOptions : google.maps.MapOptions = {
-    center : this.coordinates,
-    zoom : 17,
-  };
+  // mapOptions : google.maps.MapOptions = {
+  //   center : this.coordinates,
+  //   zoom : 17,
+  // };
 
-  marker = new google.maps.Marker({
-    position : this.coordinates,
-    map : this.map,
-  });
+  // marker = new google.maps.Marker({
+  //   position : this.coordinates,
+  //   map : this.map,
+  // });
 
-  mapInitializer () {
-    this.map = new google.maps.Map(this.gmap.nativeElement, this.mapOptions);
-    this.marker.setMap(this.map);
-  }
+  // mapInitializer () {
+  //   this.map = new google.maps.Map(this.gmap.nativeElement, this.mapOptions);
+  //   this.marker.setMap(this.map);
+  // }
 
-  ngAfterViewInit() {
-    this.mapInitializer();
-  }
+  // ngAfterViewInit() {
+  //   this.mapInitializer();
+  // }
 
   constructor(private _formBuilder : FormBuilder, private webservice : WebServiceService) { }
 
